b25b9d002fff865cccd9be06f2751f38b451a2e5
Danish <dkhans2001@gmail.com>
2025-05-24 22:55:41 -0500

    new

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,13 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      result.push(string.slice(i, j));
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
@@ -11,8 +19,16 @@ if (require.main === module) {
 
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(''));
+
+  console.log("");
+
+  console.log("Expecting: ['x', 'xy', 'xyz', 'y', 'yz', 'z']");
+  console.log("=>", consecutiveSubstrings('xyz'));
 }
 
 module.exports = consecutiveSubstrings;
-
-
